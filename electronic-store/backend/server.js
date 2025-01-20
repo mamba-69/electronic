26b871a605dcad50d8bd974f570ff7beb2b44031
mamba-69 <jennypankajpatel0511@gmail.com>
2025-01-20 15:42:55 +0000

    boom

--- electronic-store/backend/server.js
+++ electronic-store/backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('./config/db');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -8,6 +8,16 @@ const dotenv = require('dotenv');
 
 // Load environment variables
 dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 
 const app = express();
 app.use(bodyParser.json());
